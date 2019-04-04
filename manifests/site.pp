82075fa6c2d793a9b5e7544520ea842c31216dc8
Marco Siebecke <gentoomaniac@gmail.com>
2019-04-04 23:46:25 +0200

    create_ressources from yaml

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,7 @@
 node default {
   lookup('classes', Array[String], 'unique').include
+  $ressources = lookup('ressources', {})
+  $ressources.each |$key, $value| {
+    create_resources($key, $value)
+  }
 }
