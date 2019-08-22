35085c38c25a1565991903448eb426301ccbfe80
Marco Siebecke <gentoomaniac@gmail.com>
2019-08-22 07:58:45 +0200

    github_userkeys works

--- modules/hostbase/manifests/github_userkeys.pp
+++ modules/hostbase/manifests/github_userkeys.pp
@@ -1,18 +1,17 @@
-class hostbase::github_userkeys (
-  $localuser = $name,
-  $githubaccount,
-){
-  file { '/etc/userkeys/${localuser}':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => 0755,
-  }
-  file { "/etc/userkeys/${localuser}/authorized_keys":
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => 0644,
-    source => 'https://github.com/${githubaccount}.keys',
+class hostbase::github_userkeys {
+  hiera_hash('github_userkeys').each |String $localuser, String $githubaccount| {
+    file { "/etc/userkeys/${localuser}":
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755',
+    }
+    file { "/etc/userkeys/${localuser}/authorized_keys":
+      ensure => file,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => "https://github.com/${githubaccount}.keys",
+    }
   }
 }
