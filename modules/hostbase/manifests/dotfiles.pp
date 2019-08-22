257a3f6dac2cca5b51fa28f0012c7f9246dce71c
Marco Siebecke <gentoomaniac@gmail.com>
2019-08-22 19:52:52 +0000

    fix dotfiles permissions

--- modules/hostbase/manifests/dotfiles.pp
+++ modules/hostbase/manifests/dotfiles.pp
@@ -10,9 +10,18 @@ class hostbase::dotfiles
     revision => 'master',
     require  => User['marco'],
   }
+  file { '/home/marco/.vim':
+    ensure  => directory,
+    owner   => 'marco',
+    group   => 'marco',
+    recurse => true,
+    require => Vcsrepo['/home/marco/.vim'],
+  }
   file { '/home/marco/.vimrc':
     ensure  => link,
     target  => '/home/marco/.vim/.vimrc',
+    owner   => 'marco',
+    group   => 'marco',
     require => Vcsrepo['/home/marco/.vim'],
   }
 
@@ -21,20 +30,34 @@ class hostbase::dotfiles
     provider => git,
     source   => 'https://github.com/gentoomaniac/dotfiles.git',
     revision => 'master',
+    require  => User['marco'],
+  }
+  file { '/home/marco/.dotfiles':
+    ensure  => directory,
+    owner   => 'marco',
+    group   => 'marco',
+    recurse => true,
+    require => Vcsrepo['/home/marco/.dotfiles'],
   }
   file { '/home/marco/.bashrc':
     ensure  => link,
     target  => '/home/marco/.dotfiles/.bashrc',
+    owner   => 'marco',
+    group   => 'marco',
     require => Vcsrepo['/home/marco/.dotfiles'],
   }
   file { '/home/marco/.bash_completion':
     ensure  => link,
     target  => '/home/marco/.dotfiles/.bash_completion',
+    owner   => 'marco',
+    group   => 'marco',
     require => Vcsrepo['/home/marco/.dotfiles'],
   }
   file { '/home/marco/.bash_completion.d':
     ensure  => link,
     target  => '/home/marco/.dotfiles/.bashcompletion.d',
+    owner   => 'marco',
+    group   => 'marco',
     require => Vcsrepo['/home/marco/.dotfiles'],
   }
 }
