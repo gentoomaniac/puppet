a9b2a443e7b6e4c696cbff6831be240f1e12813c
Marco Siebecke <marco.siebecke@trustly.com>
2019-08-23 23:11:46 +0200

    added bind9 config

--- modules/dnsmaster/manifests/init.pp
+++ modules/dnsmaster/manifests/init.pp
@@ -7,4 +7,24 @@ class dnsmaster {
   package { $bind_packages :
     ensure => latest,
   }
+
+  file { '/etc/default/bind9':
+    ensure => file,
+    source => 'puppet:///modules/dnsmaster/defaults',
+    notify => Service['bind9'],
+  }
+
+  service { 'bind9':
+    ensure  => running,
+    enable  => true,
+    require => Package[$bind_packages],
+  }
+
+  vcsrepo { '/var/lib/dnsdata':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/gentoomaniac/dnsdata.git',
+    revision => 'master',
+    notify   => Service['bind9'],
+  }
 }
