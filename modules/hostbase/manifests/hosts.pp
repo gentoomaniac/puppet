846c936bdbe73bbbf9877e0225b7d68a181070b6
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-02 11:06:23 +0200

    use file_line for hosts to make it work on aarm64

--- modules/hostbase/manifests/hosts.pp
+++ modules/hostbase/manifests/hosts.pp
@@ -1,6 +1,8 @@
 class hostbase::hosts{
-  host { $::facts['networking']['fqdn']:
-    ensure => present,
-    ip     => $::facts['networking']['ip'],
+  file_line { "hosts.${::facts['networking']['fqdn']}":
+    ensure => 'present',
+    path   => '/etc/hosts',
+    match  => "^${::facts['networking']['ip']}.*",
+    line   => "${::facts['networking']['ip']} ${::facts['networking']['fqdn']}",
   }
 }
