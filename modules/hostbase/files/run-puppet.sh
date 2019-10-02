97670c0d0c432798c25aa7ce249a2a28f428334f
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-02 11:05:31 +0200

    do not use path but determine puppet binary location

--- modules/hostbase/files/run-puppet.sh
+++ modules/hostbase/files/run-puppet.sh
@@ -4,8 +4,9 @@ if [ "$1" != "now" ]; then
     sleep $((1 + RANDOM % 360))
 fi
 
-PATH=${PATH}:/usr/local/bin/puppet:/opt/puppetlabs/bin
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+PUPPET=$(test -f /usr/local/bin/puppet && echo /usr/local/bin/puppet || echo /opt/puppetlabs/bin/puppet)
 PUPPET_GIT_PATH=/var/lib/puppet-repo
 PUPPET_GIT_BRANCH=$(head -1 /etc/puppet_branch)
 temp_dir=$(mktemp -d -t puppet-$(date +%Y-%m-%d-%H-%M-%S)-XXX)
@@ -18,4 +19,4 @@ else
     logger -s "failed to clone puppet git repo"
 fi
 
-puppet apply --config "${PUPPET_GIT_PATH}/puppet.conf" -vvvt -l syslog "${PUPPET_GIT_PATH}/manifests/site.pp"
+${PUPPET} apply --config "${PUPPET_GIT_PATH}/puppet.conf" -vvvt -l syslog "${PUPPET_GIT_PATH}/manifests/site.pp"
