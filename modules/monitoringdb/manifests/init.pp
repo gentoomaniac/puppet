363ba445100d08952deecb82735f970fdfc1e685
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-03 16:02:07 +0200

    missing }

--- modules/monitoringdb/manifests/init.pp
+++ modules/monitoringdb/manifests/init.pp
@@ -9,10 +9,11 @@ class monitoringdb {
     restart_on_change => true,
   }
   elasticsearch::template { 'logstash_template':
-  content => {
-    'template' => 'logstash-*',
-    'settings' => {
-      'number_of_replicas' => 0,
+    content => {
+      'template' => 'logstash-*',
+      'settings' => {
+        'number_of_replicas' => 0,
+      }
     }
   }
 }
