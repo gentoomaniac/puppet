58635c66393c176292b8207dc5b6225eaecb64d8
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-03 19:08:57 +0200

    java opts

--- modules/monitoringdb/manifests/init.pp
+++ modules/monitoringdb/manifests/init.pp
@@ -4,7 +4,30 @@
 class monitoringdb {
   include ::java
 
-  class { 'elasticsearch': }
+  class { 'elasticsearch':
+    jvm_options => [
+      '-Dfile.encoding=UTF-8',
+      '-Dio.netty.noKeySetOptimization=true',
+      '-Dio.netty.noUnsafe=true',
+      '-Dio.netty.recycler.maxCapacityPerThread=0',
+      '-Djava.awt.headless=true',
+      '-Djna.nosys=true',
+      '-Dlog4j.shutdownHookEnabled=false',
+      '-Dlog4j2.disable.jmx=true',
+      '-XX:+AlwaysPreTouch',
+      '-XX:+HeapDumpOnOutOfMemoryError',
+      '-XX:+PrintGCDetails',
+      '-XX:+UseCMSInitiatingOccupancyOnly',
+      '-XX:+UseConcMarkSweepGC',
+      '-XX:-OmitStackTraceInFastThrow',
+      '-XX:CMSInitiatingOccupancyFraction=75',
+      '-Xloggc:/var/log/elasticsearch/es-01/gc.log',
+      '-Xms2g',
+      '-Xmx2g',
+      '-Xss1m',
+      '-server',
+    ]
+  }
   elasticsearch::instance { 'es-01': }
   elasticsearch::template { 'logstash_template':
     content => {
