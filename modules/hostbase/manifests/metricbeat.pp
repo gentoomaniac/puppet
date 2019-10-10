db3849a8d498e8dc11c9d98dadcf29c34071cc3e
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-10 22:26:42 +0200

    added metricbeat

---
+++ modules/hostbase/manifests/metricbeat.pp
@@ -0,0 +1,31 @@
+# Class: hostbase::metricbeat
+#
+class hostbase::metricbeat (
+  $version = latest,
+){
+  package { 'metricbeat':
+    ensure => $version,
+  }
+
+  file { '/etc/metricbeat/metricbeat.yml':
+    ensure  => file,
+    mode    => '0600',
+    source  => 'puppet:///modules/hostbase/metricbeat/metricbeat.yml',
+    require => Package['metricbeat'],
+    notify  => Service['metricbeat-svc']
+  }
+
+  file { '/etc/metricbeat/modules.d/system.yml':
+    ensure  => file,
+    mode    => '0644',
+    source  => 'puppet:///modules/hostbase/metricbeat/system.yml',
+    require => Package['metricbeat'],
+  }
+
+  service { 'metricbeat-svc':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+  }
+}
