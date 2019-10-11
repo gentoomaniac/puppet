4b21f95176174175106ec117820b17c1a87eca33
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-11 14:37:15 +0200

    added netplan config

--- modules/coredns/manifests/init.pp
+++ modules/coredns/manifests/init.pp
@@ -9,7 +9,7 @@ class coredns {
 
   file { '/etc/resolv.conf':
     ensure  => file,
-    content => 'puppet:///modules/coredns/resolv.conf',
+    source  => 'puppet:///modules/coredns/resolv.conf',
     require => Service['systemd-resolved'],
   }
 
@@ -18,17 +18,26 @@ class coredns {
   }
   file { '/opt/coredns/Corefile':
     ensure  => file,
-    content => 'puppet:///modules/coredns/Corefile',
-    require => file['/opt/coredns'],
+    source  => 'puppet:///modules/coredns/Corefile',
+    require => File['/opt/coredns'],
+  }
+
+  vcsrepo { '/opt/coredns/dnsdata':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/gentoomaniac/dnsdata.git',
+    revision => 'master',
+    notify   => [Docker::Run['coredns']],
   }
 
   docker::run { 'coredns':
     image            => 'coredns/coredns:latest',
-    ports            => ['53:53'],
+    ports            => ['53:53', '53:53/udp'],
+    command          => '-conf /data/Corefile',
     dns              => ['8.8.8.8', '8.8.4.4'],
     pull_on_start    => true,
     extra_parameters => ['--restart=unless-stopped'],
     volumes          => ['/opt/coredns:/data'],
-    require          => [File['/opt/coredns/Corefile'],Service['systemd-resolved']],
+    require          => [File['/opt/coredns/Corefile'],Service['systemd-resolved'], Vcsrepo['/opt/coredns/dnsdata']],
   }
 }
