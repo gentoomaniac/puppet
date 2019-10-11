aad984cf31c9371d1b1a07f1059fa8201dd598af
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-11 20:39:18 +0200

    remove raspberry pi code

--- modules/hostbase/manifests/puppet.pp
+++ modules/hostbase/manifests/puppet.pp
@@ -1,40 +1,19 @@
 class hostbase::puppet (
   $version = latest,
-  $aarch64_facter_version = '3.12.2.cfacter.20181217',
-  $aarch64_puppet_version = '6.9.0',
 ) {
 
-  if $facts['os']['architecture'] == 'amd64' {
-    package { 'puppet':
-      ensure => absent,
-    }
-
-    package { 'puppet-agent':
-      ensure  => "${version}-1bionic",
-      require => [Apt::Source['puppet6'],Package['puppet']],
-    }
-
-    service { 'puppet':
-      ensure  => 'stopped',
-      enable  => false,
-      require => Package['puppet-agent'],
-    }
+  package { 'puppet':
+    ensure => absent,
   }
-  elsif $facts['os']['architecture'] == 'aarch64' {
-    $dependencies = ['ruby-full', 'build-essentials', 'libboost-all-dev', 'libyaml-cpp-dev']
-    package{ $dependencies :
-      ensure => latest,
-    }
 
-    package{ 'facter':
-      ensure   => $aarch64_facter_version,
-      provider => 'gem',
-      require  => Package[$dependencies],
-    }
-    package{ 'puppet':
-      ensure   => $aarch64_puppet_version,
-      provider => 'gem',
-      require  => [Package[$dependencies], Package['facter']],
-    }
+  package { 'puppet-agent':
+    ensure  => $version,
+    require => [Apt::Source['puppet6'],Package['puppet']],
+  }
+
+  service { 'puppet':
+    ensure  => 'stopped',
+    enable  => false,
+    require => Package['puppet-agent'],
   }
 }
