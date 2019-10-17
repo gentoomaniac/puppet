cde42cf36051e34b6e623ad525db9ab923a8d8eb
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-17 22:53:06 +0200

    first stuff for logstash in docker and refactoring dockerhost

---
+++ modules/logstash_docker/manifests/init.pp
@@ -0,0 +1,34 @@
+# Class: coredns
+#
+#
+class logstash_docker {
+  file { '/srv/logstash':
+    ensure  => 'present',
+    require => File['/srv'],
+  }
+  file { '/srv/logstash/pipelines':
+    ensure  => 'present',
+    require => File['/srv/logstash'],
+  }
+  file { '/srv/logstash/config':
+    ensure  => 'present',
+    require => File['/srv/logstash'],
+  }
+
+  docker::run { 'logstash':
+    image            => 'docker.elastic.co/logstash/logstash:7.4.0',
+    ports            => [],
+    dns              => hiera('dns::servers'),
+    pull_on_start    => true,
+    extra_parameters => ['--restart=unless-stopped'],
+    volumes          => ['/srv/logstash:/usr/share/logstash/pipeline'],
+    require          => Class['docker'],
+  }
+
+  file { '/srv/logstash/config/logstash.yml':
+    ensure  => present,
+    source  => 'puppet:///modules/logstash-docker/logstash.yml',
+    require => File['/srv/logstash/config'],
+    notify  => Docker::Run['logstash'],
+  }
+}
