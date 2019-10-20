73dab9f6b9edddc4e2cf6dbb3bb313f399fa2b8f
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-20 23:15:35 +0200

    fixed loop dependecy

--- modules/logstash_docker/manifests/init.pp
+++ modules/logstash_docker/manifests/init.pp
@@ -11,6 +11,14 @@ class logstash_docker {
     require => File['/srv/logstash'],
   }
 
+  file { '/srv/logstash/config':
+    ensure  => directory,
+    source  => 'puppet:///modules/logstash_docker/config',
+    recurse => 'remote',
+    require => File['/srv/logstash'],
+    notify  => Docker::Run['logstash'],
+  }
+
   docker::run { 'logstash':
     image            => 'docker.elastic.co/logstash/logstash:7.4.0',
     ports            => ['5044:5044/tcp', '9600:9600/tcp'],
@@ -19,15 +27,5 @@ class logstash_docker {
     extra_parameters => ['--restart=unless-stopped'],
     volumes          => ['/srv/logstash/pipeline:/usr/share/logstash/pipeline', '/srv/logstash/config:/usr/share/logstash/config'],
     require          => [Class['docker'], File['/srv/logstash/config']],
-  }
-
-
-
-  file { '/srv/logstash/config':
-    ensure  => directory,
-    source  => 'puppet:///modules/logstash_docker/config',
-    recurse => 'remote',
-    require => File['/srv/logstash/config'],
-    notify  => Docker::Run['logstash'],
   }
 }
