565e320f1b3c3752f427827255b45acdd8381b64
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-25 00:11:31 +0200

    webserver for kickstart file

---
+++ modules/kickstart_nginx/manifests/init.pp
@@ -0,0 +1,30 @@
+# Class: kickstart_nginx
+#
+#
+class kickstart_nginx {
+  file { '/srv/ksnginx':
+    ensure  => directory,
+    source  => 'puppet:///modules/kickstart_nginx/data',
+    recurse => 'remote',
+    require => File['/srv'],
+    notify  => Docker::Run['ksnginx'],
+  }
+
+  docker::run { 'ksnginx':
+    image            => 'nginx',
+    expose           => ['80/tcp'],
+    dns              => hiera('dns::servers'),
+    labels           => [
+      'traefik.backend=ksnginx',
+      'traefik.port=80',
+      'traefik.enable=true',
+      'traefik.frontend.rule=Host:ks.srv.gentoomaniac.net',
+      'traefik.passHostHeader=true',
+      'traefik.frontend.whiteList.sourceRange=10.1.1.0/24,10.1.15.0/24',
+    ],
+    pull_on_start    => true,
+    extra_parameters => ['--restart=unless-stopped'],
+    volumes          => ['/srv/ksnginx:/var/tftpboot:ro'],
+    require          => [Class['docker'], File['/srv/ksnginx']],
+  }
+}
