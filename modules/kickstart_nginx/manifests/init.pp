4a46578bfa67db5b9997f3a0ca52aa4522ef8280
Marco Siebecke <marco.siebecke@trustly.com>
2019-10-25 00:24:10 +0200

    added nginx to web network

--- modules/kickstart_nginx/manifests/init.pp
+++ modules/kickstart_nginx/manifests/init.pp
@@ -14,6 +14,7 @@ class kickstart_nginx {
     image            => 'nginx',
     expose           => ['80/tcp'],
     dns              => hiera('dns::servers'),
+    net              => ['web'],
     labels           => [
       'traefik.backend=ksnginx',
       'traefik.port=80',
