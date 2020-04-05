fe3434febe8c0d2552f7a73ca2913eb13b5ed5da
Marco Siebecke <marco.siebecke@trustly.com>
2020-04-05 20:55:42 +0200

    need to clean up domain name for dir etc

--- modules/certbot/manifests/init.pp
+++ modules/certbot/manifests/init.pp
@@ -1,26 +1,29 @@
 class certbot (
-  String $domain = $name,
+  String $domain,
   Integer $interval = 80,
   String $certbot_image = 'registry.srv.gentoomaniac.net/certbot-ovh',
   String $image_tag = 'latest',
   String $email = 'marco@siebecke.se',
 ) {
-  file { "/srv/certbot-${domain}":
+  $normalized = regsubst(regsubst($domain, '\*\.', '', 'G'), '.', '_', 'G')
+
+  file { "/srv/certbot-${normalized}":
     ensure  => directory,
     require => File['/srv'],
   }
 
-  file { "/srv/certbot-${domain}/data":
+  file { "/srv/certbot-${normalized}/data":
     ensure  => directory,
-    require => File["/srv/certbot-${domain}"],
+    require => File["/srv/certbot-${normalized}"],
   }
 
   docker::image { $certbot_image:
     image_tag => $image_tag,
   }
 
-  cron::job { "certbot-cron-${domain}":
-    command     => "docker run -v '/srv/certbot-${domain}/ovh.ini:/ovh.ini:ro' -v '/srv/certbot-${domain}/data:/etc/letsencrypt' ${certbot_image} certonly --dns-ovh --dns-ovh-credentials /ovh.ini --email ${email} --agree-tos --no-eff-email -d '${domain}'",
+  $certbot_parameters = "certonly --dns-ovh --dns-ovh-credentials /ovh.ini --email ${email} --agree-tos --no-eff-email -d '${domain}'"
+  cron::job { "certbot-cron-${normalized}":
+    command     => "docker run -v '/srv/certbot-${normalized}/ovh.ini:/ovh.ini:ro' -v '/srv/certbot-${normalized}/data:/etc/letsencrypt' ${certbot_image} ${certbot_parameters}",
     minute      => '0',
     hour        => '0',
     date        => '1',
@@ -29,7 +32,7 @@ class certbot (
     user        => 'root',
     environment => ['MAILTO=root', 'PATH="/usr/bin:/bin"'],
     description => "Update ${domain} certificate every ${interval} days",
-    require     => [File["/srv/certbot-${domain}/data"],Docker::Image[$certbot_image]],
+    require     => [File["/srv/certbot-${normalized}/data"],Docker::Image[$certbot_image]],
   }
 
 }
