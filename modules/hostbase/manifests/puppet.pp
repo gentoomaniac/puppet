0efba5393914a42c832564705444f2d3551f3949
Marco Siebecke <marco.siebecke@trustly.com>
2020-04-10 23:58:16 +0200

    downgrade puppet as there is an open bug hitting PUP-10367

--- modules/hostbase/manifests/puppet.pp
+++ modules/hostbase/manifests/puppet.pp
@@ -13,7 +13,8 @@ class hostbase::puppet (
 
   package { 'puppet-agent':
     ensure  => $version,
-    require => [Apt::Source['puppet6'],Package['puppet'], Class['Apt::Update']],
+    mark    => hold,
+    require => [Apt::Source['puppet6'], Package['puppet'], Class['Apt::Update']],
   }
 
   service { 'puppet':
