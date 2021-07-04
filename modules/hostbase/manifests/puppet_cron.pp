e991fed472e1cc1190b72a0a7821cf7cfbee9697
Marco Siebecke <marco@siebecke.se>
2021-07-04 23:58:12 +0200

    use systemd timer for run-puppet

--- modules/hostbase/manifests/puppet_cron.pp
+++ modules/hostbase/manifests/puppet_cron.pp
@@ -7,16 +7,23 @@ class hostbase::puppet_cron {
     source => 'puppet:///modules/hostbase/run-puppet.sh',
   }
 
-  cron::job { 'puppet-cron':
-    command     => '/usr/local/bin/run-puppet',
-    minute      => '*/30',
-    hour        => '*',
-    date        => '*',
-    month       => '*',
-    weekday     => '*',
-    user        => 'root',
-    environment => ['MAILTO=root', 'PATH="/usr/bin:/bin:/opt/puppetlabs/bin"'],
-    description => 'Run Puppet every 30m',
-    require     => File['/usr/local/bin/run-puppet'],
+  file { '/etc/systemd/system/run-puppet.service':
+    ensure  => 'present',
+    mode    => '0744',
+    source  => 'puppet:///modules/hostbase/puppet.service',
+    require => File['/usr/local/bin/run-puppet'],
+  }
+
+  file { '/etc/systemd/system/run-puppet.timer':
+    ensure  => 'present',
+    mode    => '0744',
+    source  => 'puppet:///modules/hostbase/puppet.timer',
+    require => File['/etc/systemd/system/run-puppet.service'],
+  }
+
+ service { "run-puppet.timer":
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/systemd/system/run-puppet.timer'],
   }
 }
