5c326c1caf2de011da2efa26350d228260981c5f
Marco Siebecke <marco@siebecke.se>
2024-09-26 23:59:51 +0200

    allow for package removal

--- modules/hostbase/manifests/packages.pp
+++ modules/hostbase/manifests/packages.pp
@@ -1,6 +1,11 @@
 class hostbase::packages (){
-  $packages = lookup('hostbase::packages::base_packages', Array[String], 'unique')
+  $packages = lookup('hostbase::packages::present', Array[String], 'unique')
   package { $packages :
     ensure => latest,
+  }
+
+  $remove = lookup('hostbase::packages::absent', Array[String], 'unique')
+  package { $remove :
+    ensure => absent,
   }
 }
