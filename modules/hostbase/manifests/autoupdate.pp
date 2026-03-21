8ce74c1bd27a84e2e680706748b5321c8111b3b8
Marco Siebecke <marco@siebecke.se>
2026-03-21 21:48:25 +0100

    autoupdates on arch

--- modules/hostbase/manifests/autoupdate.pp
+++ modules/hostbase/manifests/autoupdate.pp
@@ -1,15 +1,37 @@
-
 class hostbase::autoupdate {
-  package { 'unattended-upgrades':
-    ensure => latest,
+
+  file { '/etc/systemd/system/pacman-update.service':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/hostbase/pacman-update.service',
+    notify => Exec['pacman_update_daemon_reload'],
   }
 
-  file { '/etc/apt/apt.conf.d/50unattended-upgrades':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/hostbase/50unattended-upgrades',
-    require => Package['unattended-upgrades'],
+  file { '/etc/systemd/system/pacman-update.timer':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/hostbase/pacman-update.timer',
+    notify => Exec['pacman_update_daemon_reload'],
+  }
+
+  if !defined(Exec['pacman_update_daemon_reload']) {
+    exec { 'pacman_update_daemon_reload':
+      command     => '/usr/bin/systemctl daemon-reload',
+      refreshonly => true,
+    }
+  }
+
+  service { 'pacman-update.timer':
+    ensure    => running,
+    enable    => true,
+    require   => [
+      File['/etc/systemd/system/pacman-update.service'],
+      File['/etc/systemd/system/pacman-update.timer']
+    ],
+    subscribe => Exec['pacman_update_daemon_reload'],
   }
 }
