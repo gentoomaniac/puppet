73fa012abfd571be67e8cd48999ceea61001d95b
Marco Siebecke <marco@siebecke.se>
2026-03-28 18:45:41 +0100

    move docker class into site to avoid race conditions that break unsupported mode

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node default {
+  class { 'docker':
+    manage_package             => false,
+    acknowledge_unsupported_os => true,
+  }
+
   lookup('classes', Array[String], 'unique', []).include
   $ressources = lookup('ressources', Hash, 'deep', {})
   $ressources.each |$key, $value| {
