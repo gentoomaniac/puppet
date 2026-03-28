4d4c5776c4d2be60ab0d99d6e68fd445df2984cd
Marco Siebecke <marco@siebecke.se>
2026-03-28 22:33:29 +0100

    coredns-c1 move

--- modules/coredns/manifests/init.pp
+++ modules/coredns/manifests/init.pp
@@ -2,14 +2,13 @@
 #
 #
 class coredns {
-  zfs { 'datapool/coredns':
+  btrfs::subvolume { '/srv/coredns':
     ensure => present,
   }
   file { '/srv/coredns/Corefile':
     ensure  => file,
     source  => 'puppet:///modules/coredns/Corefile',
-    require => Zfs['datapool/coredns'],
-    notify  => Docker::Run['coredns'],
+    require => Btrfs::Subvolume['/srv/coredns'],
   }
 
   vcsrepo { '/srv/coredns/dnsdata':
@@ -17,8 +16,7 @@ class coredns {
     provider => git,
     source   => 'https://github.com/gentoomaniac/dnsdata.git',
     revision => 'master',
-    require => Zfs['datapool/coredns'],
-    notify   => Docker::Run['coredns'],
+    require => Btrfs::Subvolume['/srv/coredns'],
   }
 
   docker::run { 'coredns':
@@ -29,6 +27,6 @@ class coredns {
     pull_on_start    => true,
     extra_parameters => ['--restart=unless-stopped'],
     volumes          => ['/srv/coredns:/data'],
-    require          => [File['/srv/coredns/Corefile'],Service['systemd-resolved'], Vcsrepo['/srv/coredns/dnsdata'], Class['docker']],
+    require          => [File['/srv/coredns/Corefile'],Vcsrepo['/srv/coredns/dnsdata']],
   }
 }
