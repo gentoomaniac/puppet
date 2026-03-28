9b34bff4db260c8e78e4f2949abddb2c693424e1
Marco Siebecke <marco@siebecke.se>
2026-03-29 00:27:53 +0100

    move ns1 to arch

--- modules/kickstart_nginx/files/data/arch/setup.sh
+++ modules/kickstart_nginx/files/data/arch/setup.sh
@@ -3,7 +3,7 @@
 # get bootstrap config from /proc/cmdline
 KICKSTART_BASE_URL=$(sed -n 's/.*KICKSTART_BASE_URL=\([^ ]\+\).*/\1/p' /proc/cmdline)
 KICKSTART_BASE_URL=${KICKSTART_BASE_URL:-https://ks.srv.gentoomaniac.net}
-PUPPET_BRANCH=$(sed -n 's/.*PUPPET_BRANCH=\([a-zA-Z0-9\._-/]\+\).*/\1/p' /proc/cmdline)
+PUPPET_BRANCH=$(sed -n 's/.*PUPPET_BRANCH=\([a-zA-Z0-9\._-]\+\).*/\1/p' /proc/cmdline)
 VAULT_TOKEN=$(sed -n 's/.*VAULT_TOKEN=\([a-zA-Z0-9\.]\+\).*/\1/p' /proc/cmdline)
 
 # Run archinstall and catch failures!
